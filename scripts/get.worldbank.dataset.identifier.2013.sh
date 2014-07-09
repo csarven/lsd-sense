46aa3ddc77762461909c38d50d76389c0b90d677
Sarven Capadisli <info@csarven.ca>
2014-07-09 18:11:32 +0200

    Minor

--- scripts/get.worldbank.dataset.identifier.2013.sh
+++ scripts/get.worldbank.dataset.identifier.2013.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 curl -v -G http://worldbank.270a.info/sparql \
---data-urlencode "query@worldbank.dataset-identifiers.2013.q" \
+--data-urlencode "query@worldbank.dataset.identifier.2013.q" \
 --data "output=csv" \
--o ../data/worldbank.dataset-identifiers.2013.csv
+-o ../data/worldbank.dataset.identifier.2013.csv
