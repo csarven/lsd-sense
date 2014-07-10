2746c05f79270d37eb90c851e9b3dd9cd5919176
Sarven Capadisli <info@csarven.ca>
2014-07-10 19:33:22 +0200

    Using CSV

--- scripts/get.worldbank.refArea.obsValue.sh
+++ scripts/get.worldbank.refArea.obsValue.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 data="../data/worldbank.dataset.identifier.2013.csv";
+refPeriod="2013";
 
 tail -n +2 "$data" | tr -d '\r' | while read dataSetCode ; do
-curl -v -L -H "Accept: application/sparql-results+xml" \
+echo "GET ${dataSetCode} ${refPeriod} observations";
+#curl -L -H "Accept: application/sparql-results+xml" \
+curl -s -L -H "Accept: text/csv" \
     -G http://worldbank.270a.info/sparql \
     --data-urlencode "query=SELECT ?refArea ?obsValue
 WHERE
   { ?observation <http://purl.org/linked-data/cube#dataSet> <http://worldbank.270a.info/dataset/"${dataSetCode}"> .
-    ?observation <http://purl.org/linked-data/sdmx/2009/dimension#refPeriod> <http://reference.data.gov.uk/id/year/2013> .
-    ?observation <http://purl.org/linked-data/sdmx/2009/dimension#refArea> ?refArea .
+    ?observation <http://purl.org/linked-data/sdmx/2009/dimension#refPeriod> <http://reference.data.gov.uk/id/year/${refPeriod}> .
+    ?observation <http://purl.org/linked-data/sdmx/2009/dimension#refArea>/<http://www.w3.org/2004/02/skos/core#notation> ?refArea .
     ?observation <http://purl.org/linked-data/sdmx/2009/measure#obsValue> ?obsValue .
   }" \
-
-#Outputting this for now to test. In actual tests, this need not be stored since ATS will cache everything for the analysis
-    -o ../data/observations/"${dataSetCode}".2013.xml;
+    --data "output=csv" \
+    -o ../data/observations/"${dataSetCode}"."${refPeriod}".csv;
 done;
 
-#real	3m6.052s
-#user	0m4.585s
-#sys	0m3.366s
+#real	2m18.357s
+#user	0m4.614s
+#sys	0m2.958s
 
